d1854af4327ef3faeb60debf0fc2ff248afa854f
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-01-03 01:25:09 +0530

    second-commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -118,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
+STATIC_URL = "/static/"
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"carzone/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
