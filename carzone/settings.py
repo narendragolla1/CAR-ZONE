0b61e68c4faf38dcdce0561d151965d46c2c1dae
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-01-02 19:37:34 +0530

    CAR_ZONE

--- carzone/settings.py
+++ carzone/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+   
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
-    # return HttpResponse('<h1>hello world</h1>')
     return render(request, 'pages/home.html')
