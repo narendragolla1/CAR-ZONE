d831a41fcd585b0bf3f6ff34d0e0f54f8677e80a
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-01-07 11:50:38 +0530

    created models for cars

--- carzone/urls.py
+++ carzone/urls.py
@@ -18,7 +18,9 @@ from django.urls import path,include
 from pages import urls
 from django.conf.urls.static import static
 from django.conf import settings
+from cars import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('cars',include('cars.urls'))
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
